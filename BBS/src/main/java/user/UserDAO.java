fe70d3f192802503d82274b41eb208ba9ca06a77
yongheon <jung09965555@gmail.com>
2024-05-16 20:59:40 +0900

    add joinAction.jsp update UserDAO.java

--- BBS/src/main/java/user/UserDAO.java
+++ BBS/src/main/java/user/UserDAO.java
@@ -53,6 +53,20 @@ public class UserDAO {
 			return -2; //데이터베이스 오류
 		}
 		
-		
+		public int join(User user) {
+			  String sql = "insert into bbs_user values(?, ?, ?, ?, ?)";
+			  try {
+			    pstmt = conn.prepareStatement(sql);
+			    pstmt.setString(1, user.getUserID());
+			    pstmt.setString(2, user.getUserPassword());
+			    pstmt.setString(3, user.getUserName());
+			    pstmt.setString(4, user.getUserGender());
+			    pstmt.setString(5, user.getUserEmail());
+			    return pstmt.executeUpdate();
+			  }catch (Exception e) {
+			 	e.printStackTrace();
+			  }
+			  return -1;
+			}
 	
 }
